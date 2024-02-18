674a04c536eccf8dd9f858d6beca6ae4f10f0325
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 08:52:28 +0100

    feat:"Relaciones"

--- examen1/app/Models/Type.php
+++ examen1/app/Models/Type.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Task;
 
 class Type extends Model
 {
@@ -13,4 +15,18 @@ class Type extends Model
         'colour',
         'model',
     ];
+
+ // Relación uno a muchos con Task
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    // Relación uno a muchos con User
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
