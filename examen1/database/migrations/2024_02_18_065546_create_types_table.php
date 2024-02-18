56266df18ef7f638953826e18edcafb0af666429
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 08:47:31 +0100

    fix:"error profesor"

--- examen1/database/migrations/2024_02_18_065546_create_types_table.php
+++ examen1/database/migrations/2024_02_18_065546_create_types_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('colour')->unique();
-            $table->string('model')->unique();
+            $table->string('model');
             $table->timestamps();
         });
     }
