5a6dbca58e8a396eb0c8603cf9fdca93ea32b3d5
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 07:58:56 +0100

    feat:"cambios migracion"

--- examen1/database/migrations/2024_02_18_065546_create_types_table.php
+++ examen1/database/migrations/2024_02_18_065546_create_types_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('colour')->unique();
+            $table->string('model')->unique();
             $table->timestamps();
         });
     }
