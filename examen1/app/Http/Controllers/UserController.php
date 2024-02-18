7ae9bc40d123e7103925cf7beda5d69c9b25796b
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 10:46:41 +0100

    fix:"error relacion"

--- examen1/app/Http/Controllers/UserController.php
+++ examen1/app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all()->with('types');
+        $users = User::all();
         return view('users.index', compact('users'));
     }
 
