2688e6760634ccaf98f144b720a431914ecfe55e
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 08:13:32 +0100

    fix:"Errores en modelos"

--- examen1/app/Models/Type.php
+++ examen1/app/Models/Type.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Types extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'colour',
+        'model',
+    ]
 }
