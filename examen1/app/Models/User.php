348eaa9d2f522aed5363490b10824a5d453c5e47
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 11:04:59 +0100

    fix:"relation error"

--- examen1/app/Models/User.php
+++ examen1/app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
 
     public function type()
     {
-        return $this->belongTo(Type::class);
+        return $this->belongsTo(Type::class);
     }
 
     public function tasks()
