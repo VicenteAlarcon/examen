3c023f2b37804618d4ecc24ff6370c8f40a97afc
Vicente Alarcon <alarcongonzalezvicente@gmail.com>
2024-02-18 09:32:00 +0100

    fix:"Migrations error"

--- examen1/database/migrations/2024_02_18_083109_update_to_users_table.php
+++ examen1/database/migrations/2024_02_18_083109_update_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
         });
     }
 
